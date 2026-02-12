baedb355e50b404874c9a63c9ec6b4aec422b233
Tygels <tomstar0027@gmail.com>
2026-02-12 19:55:14 +0100

    .

--- Scenes/Login/Background.gd
+++ Scenes/Login/Background.gd
@@ -2,4 +2,4 @@ extends ColorRect
 
 func _ready() -> void:
 	set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
-	color = Color("#1e1f2b") # dunkler, moderner Hintergrund
+	color = Color("#1e1f2b")
